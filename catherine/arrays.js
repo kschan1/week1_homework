bdf04aa765189dc770edf8e1c1359951dd41aaa6
Catherine Gracey <catherine.gracey@gmail.com>
2016-09-27 16:03:59 +1000

    arrays exercise

---
+++ catherine/arrays.js
@@ -0,0 +1,6 @@
+var faveColours = ['purple', 'pink', 'blue', 'green'];
+var endings = ['1st', '2nd', '3rd', '4th'];
+
+for (var i = 0; i < faveColours.length; i++) {
+  console.log('My ' + endings[i] + ' choice is ' + faveColours[i] + '.');
+}
