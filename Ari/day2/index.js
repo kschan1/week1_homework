9cac70064e1ec312da1aaea013c0d4272c15571a
Ari Crosby <REXB@Aris-MacBook-Pro.local>
2016-09-28 15:16:57 +1000

    d2 homework

---
+++ Ari/day2/index.js
@@ -0,0 +1,24 @@
+/*
+Part 1
+console.log('hello');
+
+var name = prompt("What is your name?")
+
+console.log("hello" + "," + " " + name + "!");
+
+
+Part 2
+*/
+for (i = 0; i <= 10; i++)
+  console.log(i + " times 9 = " + ( i * 9));
+
+/*Part 3
+
+var dinnerMenu = prompt("On the menu tonight we have: (1)Steak, (2)Fruit salad, (3)Tofurkey or (4)Pork chops. Which one would you like? 1, 2, 3 or 4?")
+
+
+
+var Steak =
+var fruit salad
+var tofurkey
+var pork chops*/
